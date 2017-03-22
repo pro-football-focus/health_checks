fc5e199af5cb908c603ddd20d3ef996ddc77e269
Jake Weaver <jweaver60@gmail.com>
2017-03-22 15:46:07 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HealthChecks.Mixfile do
 
   def project do
     [app: :health_checks,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
